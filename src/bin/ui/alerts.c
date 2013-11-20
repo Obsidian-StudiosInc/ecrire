783e3bed59834e3c1d32ca6aa04263e38c91a3c1
Tom Hacohen <tom@stosb.com>
2013-11-20 12:39:17 +0000

    Used elm_popup instead of elm_inwin.

--- src/bin/ui/alerts.c
+++ src/bin/ui/alerts.c
@@ -38,20 +38,20 @@ void
 ui_alert_need_saving(Evas_Object *entry, void (*done)(void *data), void *data)
 {
    Ecrire_Entry *ent = data;
-   Evas_Object *inwin, *bx, *hbx, *btn, *lbl;
-   inwin = elm_win_inwin_add(elm_object_top_widget_get(entry));
-   evas_object_show(inwin);
+   Evas_Object *popup, *bx, *hbx, *btn, *lbl;
+   popup = elm_popup_add(elm_object_top_widget_get(entry));
+   evas_object_show(popup);
 
    done_cb = done;
    done_data = data;
 
-   bx = elm_box_add(inwin);
-   elm_win_inwin_content_set(inwin, bx);
+   bx = elm_box_add(popup);
+   elm_object_content_set(popup, bx);
    evas_object_size_hint_weight_set(bx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(bx, EVAS_HINT_FILL, 0.0);
    evas_object_show(bx);
 
-   lbl = elm_label_add(inwin);
+   lbl = elm_label_add(popup);
    elm_object_text_set(lbl,
          _("<align=center>Would you like to save changes to document?<br>"
          "Any unsaved changes will be lost."));
@@ -60,33 +60,33 @@ ui_alert_need_saving(Evas_Object *entry, void (*done)(void *data), void *data)
    elm_box_pack_end(bx, lbl);
    evas_object_show(lbl);
 
-   hbx = elm_box_add(inwin);
+   hbx = elm_box_add(popup);
    elm_box_horizontal_set(hbx, EINA_TRUE);
    evas_object_size_hint_weight_set(hbx, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
    evas_object_size_hint_align_set(hbx, EVAS_HINT_FILL, 0.0);
    elm_box_pack_end(bx, hbx);
    evas_object_show(hbx);
 
-   btn = elm_button_add(inwin);
+   btn = elm_button_add(popup);
    elm_object_text_set(btn, _("Save"));
    elm_box_pack_end(hbx, btn);
    evas_object_show(btn);
-   evas_object_smart_callback_add(btn, "clicked", _save, inwin);
+   evas_object_smart_callback_add(btn, "clicked", _save, popup);
 
    if (elm_object_item_disabled_get(ent->save_item) || !ent->filename)
      {
         elm_object_disabled_set(btn, EINA_TRUE);
      }
 
-   btn = elm_button_add(inwin);
+   btn = elm_button_add(popup);
    elm_object_text_set(btn, _("Discard"));
    elm_box_pack_end(hbx, btn);
    evas_object_show(btn);
-   evas_object_smart_callback_add(btn, "clicked", _discard, inwin);
+   evas_object_smart_callback_add(btn, "clicked", _discard, popup);
 
-   btn = elm_button_add(inwin);
+   btn = elm_button_add(popup);
    elm_object_text_set(btn, _("Cancel"));
    elm_box_pack_end(hbx, btn);
    evas_object_show(btn);
-   evas_object_smart_callback_add(btn, "clicked", _cancel, inwin);
+   evas_object_smart_callback_add(btn, "clicked", _cancel, popup);
 }
