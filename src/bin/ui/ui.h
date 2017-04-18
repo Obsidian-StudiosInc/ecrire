00c9e3c4f875d5f88e9c0a0a7ea82c643c4aef01
William L. Thomson Jr <wlt@o-sinc.com>
2017-04-18 18:50:33 -0400

    Ecrire: Updated ui.h for settings dialog

--- src/bin/ui/ui.h
+++ src/bin/ui/ui.h
@@ -9,6 +9,7 @@ Evas_Object *ui_find_dialog_open(Evas_Object *parent, Ecrire_Entry *ent);
 Evas_Object *ui_goto_dialog_open(Evas_Object *parent, Ecrire_Entry *ent);
 
 Evas_Object *ui_font_dialog_open(Evas_Object *parent, Ecrire_Entry *ent, const char *pfont, int size);
+Evas_Object *ui_settings_dialog_open(Evas_Object *parent, Ecrire_Entry *ent,Ent_Cfg *ent_cfg);
 
 void ui_alert_need_saving(Evas_Object *entry, void (*done)(void *data), void *data);
 
