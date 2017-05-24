7814b5de1b40d7c6a7e940b7ca694d5410c0f63c
William L. Thomson Jr <wlt@o-sinc.com>
2017-05-23 21:11:56 -0400

    Fix issue with replace all causing crash, seems to be speed related?

--- src/bin/ui/search_dialog.c
+++ src/bin/ui/search_dialog.c
@@ -152,7 +152,8 @@ _replace_all_clicked(void *data,
                      Evas_Object *obj EINA_UNUSED,
                      void *event_info EINA_UNUSED)
 {
-    while(_replace_in_entry((Ecrire_Entry *)data)==EINA_TRUE) ;
+  /* FIXME: Added temp hack to slow down loop, to fast causes crash? */
+  while(_replace_in_entry((Ecrire_Entry *)data)==EINA_TRUE) sleep(0.1);
 }
 
 static void
