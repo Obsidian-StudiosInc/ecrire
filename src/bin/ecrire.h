e2a4004d81b88e8b4017e28babc9eafa707b55e6
William L. Thomson Jr <wlt@o-sinc.com>
2017-05-23 21:35:55 -0400

    Begin rename of Ecrire_Entry -> Ecrire_Doc, deprecate Ecrire_Entry
    No longer using elm_entry, and the struc represents a document
    essentially so the new name is better suited.

--- src/bin/ecrire.h
+++ src/bin/ecrire.h
@@ -14,7 +14,7 @@ extern int _ecrire_log_dom;
 
 #define ALPHA(O,A) evas_object_color_set (O, 255, 255, 255, A)
 
-struct _Ecrire_Entry {
+struct _Ecrire_Doc {
      Elm_Code *code;
      Elm_Code_Widget *entry;
      Evas_Object *win;
@@ -22,22 +22,25 @@ struct _Ecrire_Entry {
      Evas_Object *box_editor;
      Evas_Object *box_main;
      Evas_Object *cursor_label;
-     const char *filename;
-     int unsaved;
+     /* Main Menu Items */
      Elm_Object_Item *close_item, *copy_item, *cut_item, *save_item,
                      *save_as_item, *paste_item, *undo_item, *redo_item;
+     const char *filename;
+     int unsaved;
 };
 
-typedef struct _Ecrire_Entry Ecrire_Entry;
+typedef struct _Ecrire_Doc Ecrire_Doc;
+/* Deprecated */
+typedef struct _Ecrire_Doc Ecrire_Entry;
 
 char *_load_plain(const char *file);
 Eina_Bool _load_file(const char *file, const Elm_Code_File *code_file);
 Eina_Bool _save_markup_utf8(const char *file, const char *text);
 Eina_Bool _save_plain_utf8(const char *file, const char *text);
 
-void editor_font_set(Ecrire_Entry *ent, const char *name, unsigned int size);
-void editor_save(Ecrire_Entry *ent, void *callback_func);
-void save_do(const char *file, Ecrire_Entry *ent);
+void editor_font_set(Ecrire_Doc *doc, const char *name, unsigned int size);
+void editor_save(Ecrire_Doc *doc, void *callback_func);
+void save_do(const char *file, Ecrire_Doc *doc);
 
 #ifdef ENABLE_NLS
 # include <libintl.h>
