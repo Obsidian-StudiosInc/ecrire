a7960c69b16f7e93208788f6859a0566c54616aa
William L. Thomson Jr <wlt@o-sinc.com>
2017-05-23 21:59:05 -0400

    Rename Ecrire_Entry *ent -> Ecrire_Doc *doc

--- src/bin/ui/ui.h
+++ src/bin/ui/ui.h
@@ -5,11 +5,11 @@
 
 void ui_file_open_save_dialog_open(Evas_Object *parent, Evas_Smart_Cb func, Eina_Bool save);
 
-Evas_Object *ui_find_dialog_open(Evas_Object *parent, Ecrire_Entry *ent);
-Evas_Object *ui_goto_dialog_open(Evas_Object *parent, Ecrire_Entry *ent);
+Evas_Object *ui_find_dialog_open(Evas_Object *parent, Ecrire_Doc *doc);
+Evas_Object *ui_goto_dialog_open(Evas_Object *parent, Ecrire_Doc *doc);
 
-Evas_Object *ui_font_dialog_open(Evas_Object *parent, Ecrire_Entry *ent, const char *pfont, int size);
-Evas_Object *ui_settings_dialog_open(Evas_Object *parent, Ecrire_Entry *ent,Ent_Cfg *ent_cfg);
+Evas_Object *ui_font_dialog_open(Evas_Object *parent, Ecrire_Doc *doc, const char *pfont, int size);
+Evas_Object *ui_settings_dialog_open(Evas_Object *parent, Ecrire_Doc *doc,Ent_Cfg *ent_cfg);
 
 void ui_alert_need_saving(Evas_Object *entry, void (*done)(void *data), void *data);
 
