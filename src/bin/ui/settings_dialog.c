4d3c320fefabe4a82145701a7dc92ee2968e76e1
William L. Thomson Jr <wlt@o-sinc.com>
2017-10-19 16:27:29 -0400

    bin: Auto save on change of settings, remove ok/apply buttons
    Changes were applied regardless of apply, only saved on apply. Which
    could be wanted or misleading. Ok and close were identical in function,
    redundant buttons for no benefit.

--- src/bin/ui/settings_dialog.c
+++ src/bin/ui/settings_dialog.c
@@ -37,14 +37,7 @@ settings_alpha_cb (void *data,
   ALPHA (doc->bg, ent_cfg->alpha);
   ALPHA (doc->box_main, ent_cfg->alpha);
   ALPHA (doc->box_editor, ent_cfg->alpha);
-}
-
-static void
-settings_apply_cb (void *data EINA_UNUSED,
-                   Evas_Object *obj EINA_UNUSED,
-                   void *event_info EINA_UNUSED)
-{
-   ecrire_cfg_save();
+  ecrire_cfg_save();
 }
 
 static void
@@ -59,6 +52,7 @@ settings_apply_font_cb (void *data,
   if(name)
     ent_cfg->font.name = name;
   ent_cfg->font.size = size;
+  ecrire_cfg_save();
 }
 
 static void
@@ -86,6 +80,7 @@ settings_default_font_cb(void *data,
       elm_obj_code_widget_font_set(doc->widget, NULL, 10);
       ent_cfg->font.name = NULL;
       ent_cfg->font.size = 10;
+      ecrire_cfg_save();
     }
   else
     settings_apply_font_cb(doc, (Evas_Object *)NULL, (void *)NULL);
@@ -132,6 +127,7 @@ settings_line_numbers_cb (void *data,
   state = elm_check_state_get (obj);
   elm_obj_code_widget_line_numbers_set ((Elm_Code_Widget *)data, state);
   ent_cfg->line_numbers = !state;
+  ecrire_cfg_save();
 }
 
 static void
@@ -145,6 +141,7 @@ settings_menu_cb (void *data, Evas_Object *obj, void *event_info EINA_UNUSED)
   if(!state)
     evas_object_del(elm_win_main_menu_get(win));
   ent_cfg->menu = !state;
+  ecrire_cfg_save();
 }
 
 static void
@@ -158,6 +155,7 @@ settings_toolbar_cb (void *data, Evas_Object *obj, void *event_info EINA_UNUSED)
   else
     evas_object_del(((Ecrire_Doc *)data)->toolbar);
   ent_cfg->toolbar = !state;
+  ecrire_cfg_save();
 }
 
 static void
@@ -175,6 +173,7 @@ settings_word_wrap_cb (void *data,
       elm_entry_line_wrap_set ((Evas_Object *)data, ELM_WRAP_NONE);
       ent_cfg->wrap_type = ELM_WRAP_NONE;
     }
+  ecrire_cfg_save();
 }
 
 Evas_Object *
@@ -379,36 +378,6 @@ ui_settings_dialog_open(Evas_Object *parent,
     }
   evas_object_smart_callback_add(list, "selected", settings_apply_font_cb, doc);
 
-  /* Ok Button */
-  obj = elm_button_add(table);
-  elm_object_text_set(obj,_("OK"));
-  ic = elm_image_add (win);
-  if (elm_icon_standard_set (ic,"dialog-ok")) {
-    elm_object_content_set (obj, ic);
-    evas_object_show(ic);
-  } else
-    evas_object_del(ic);
-  evas_object_size_hint_align_set(obj, 1, 0);
-  elm_table_pack (table, obj, 0, row, 1, 1);
-  evas_object_size_hint_min_set(obj, ELM_SCALE_SIZE(BUTTON_WIDTH), ELM_SCALE_SIZE(BUTTON_HEIGHT));
-  evas_object_smart_callback_add (obj, "clicked", settings_delete_cb, win);
-  evas_object_show (obj);
-  
-  /* Apply Button */
-  obj = elm_button_add(table);
-  elm_object_text_set(obj,_("Apply"));
-  ic = elm_image_add (win);
-  if (elm_icon_standard_set (ic,"dialog-apply")) {
-    elm_object_content_set (obj, ic);
-    evas_object_show(ic);
-  } else
-    evas_object_del(ic);
-  evas_object_size_hint_align_set(obj, 0, 0);
-  elm_table_pack (table, obj, 1, row, 1, 1);
-  evas_object_size_hint_min_set(obj, ELM_SCALE_SIZE(BUTTON_WIDTH), ELM_SCALE_SIZE(BUTTON_HEIGHT));
-  evas_object_smart_callback_add (obj, "clicked", settings_apply_cb, NULL);
-  evas_object_show (obj);
-
   /* Close Button */
   obj = elm_button_add(table);
   elm_object_text_set(obj,_("Close"));
@@ -418,8 +387,8 @@ ui_settings_dialog_open(Evas_Object *parent,
     evas_object_show(ic);
   } else
     evas_object_del(ic);
-  evas_object_size_hint_align_set(obj, 0, 0);
-  elm_table_pack (table, obj, 2, row, 1, 1);
+  evas_object_size_hint_align_set(obj, 0.5, 0);
+  elm_table_pack (table, obj, 0, row, 3, 1);
   evas_object_size_hint_min_set(obj, ELM_SCALE_SIZE(BUTTON_WIDTH), ELM_SCALE_SIZE(BUTTON_HEIGHT));
   evas_object_smart_callback_add (obj, "clicked", settings_delete_cb, win);
   evas_object_show (obj);
