bbf1216177518445e6f9948e6c0b0bd6b07337d6
William L. Thomson Jr <wlt@o-sinc.com>
2018-09-07 18:39:19 -0400

    bin: Change order of prototypes

--- src/bin/ui/ui.h
+++ src/bin/ui/ui.h
@@ -3,9 +3,6 @@
 
 #include "../ecrire.h"
 
-void ui_file_open_save_dialog_open(Ecrire_Doc *doc,
-                                   Evas_Smart_Cb func,
-                                   Eina_Bool save);
 Evas_Object *ui_find_dialog_open(Evas_Object *parent, Ecrire_Doc *doc);
 Evas_Object *ui_settings_dialog_open(Evas_Object *parent,
                                      Ecrire_Doc *doc,
@@ -13,5 +10,8 @@ Evas_Object *ui_settings_dialog_open(Evas_Object *parent,
 void ui_alert_need_saving(Evas_Object *entry,
                           void (*done)(void *data),
                           void *data);
+void ui_file_open_save_dialog_open(Ecrire_Doc *doc,
+                                   Evas_Smart_Cb func,
+                                   Eina_Bool save);
 
 #endif
